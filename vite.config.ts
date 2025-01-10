b0d78a3d6d86f23d28ec908747f7561ff5aa673a
W3Router <minzf581@gmail.com>
2025-01-10 12:16:16 +0800

    fix: update GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/IPProxyweb/', 
+  base: '/IPProxy/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
